46f1167be5d330664465e1e35f08f6aa537cf609
Gus Geurts <Gus Geurts@192.168.0.157>
2020-12-20 02:17:32 +0100

    primary en secondary gedelete en speelveld.fxml aangemaakt

---
+++ src/main/java/be/inf1/iiw1b/pac/xon_mgm_inf1_iiw1ba2021/SpeelveldController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.inf1.iiw1b.pac.xon_mgm_inf1_iiw1ba2021;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Gus Geurts
+ */
+public class SpeelveldController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module be.inf1.iiw1b.pac.xon_mgm_inf1_iiw1ba2021 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens be.inf1.iiw1b.pac.xon_mgm_inf1_iiw1ba2021 to javafx.fxml;
     exports be.inf1.iiw1b.pac.xon_mgm_inf1_iiw1ba2021;
